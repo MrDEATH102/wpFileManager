96109d626df31df60f475c22c2483773d391e3e2
Arman <69141589+MrDEATH102@users.noreply.github.com>
2025-06-09 16:21:40 +0330

    Update FAM_VERSION constant to 1.4.6

--- file-archive-manager.php
+++ file-archive-manager.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 }
 
 define('FAM_PLUGIN_DIR', plugin_dir_path(__FILE__));
-define('FAM_VERSION', '1.0.0');
+define('FAM_VERSION', '1.4.6');
 define('FAM_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('FAM_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
